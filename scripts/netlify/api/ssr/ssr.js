f29e925e099e8499621e95800edea9538b8da3e5
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-04-10 11:11:56 +0200

    try LAMBDA_TASK_ROOT

--- scripts/netlify/api/ssr/ssr.js
+++ scripts/netlify/api/ssr/ssr.js
@@ -1,8 +1,12 @@
 const fs = require('fs')
 const { ssr } = require('@sveltech/ssr')
 const distDir = '../../../../dist/'
-const script = fs.readFileSync(`${distDir}build/bundle.js`, 'utf8')
-const template = fs.readFileSync(`${distDir}__app.html`, 'utf8')
+const path = require('path')
+
+const root = process.env.LAMBDA_TASK_ROOT || __dirname
+
+const script = path.resolve(root, distDir, 'build/bundle.js')
+const template = path.resolve(root, distDir,'__app.html')
 
 exports.handler = async (event, context) => {
     const body = await ssr(template, script, event.path)
